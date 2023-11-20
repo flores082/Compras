9c6f75629c73821c9accf7ba3b8f80bd8f2ad052
Michel Flores <florespoblete17@gmail.com>
2023-11-20 12:42:36 -0300

    cambios a realisar

--- app/src/main/java/com/example/flores/compras/Inicio.kt
+++ app/src/main/java/com/example/flores/compras/Inicio.kt
@@ -1,7 +1,13 @@
 package com.example.flores.compras
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.graphics.Color
 import android.media.MediaPlayer
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -22,12 +28,13 @@ class Inicio : AppCompatActivity() {
 
     var Lista_C = mutableListOf<Lista_Compras>()
     lateinit var recyclerView:RecyclerView
+    var idiomaActual : String = "es"
 
-    private var idiomaActual: String = "es"
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
         mediaPlayer = MediaPlayer.create(this, R.raw.precionar)
 
         recyclerView = findViewById<RecyclerView>(R.id.Lista_p)
@@ -40,12 +47,12 @@ class Inicio : AppCompatActivity() {
     private fun agregar() {
         // Implementar lógica para agregar
         mediaPlayer.start()
-        val nombreProducto = getString(R.string.producto)
+        /*val nombreProducto = getString(R.string.producto)
         val cantidadProducto = getString(R.string.cantidad)
         val precioProducto = getString(R.string.precio)
         val item = Lista_Compras(nombreProducto,cantidadProducto,precioProducto)
         Lista_C.add(item)
-        recyclerView.adapter?.notifyDataSetChanged()
+        recyclerView.adapter?.notifyDataSetChanged()*/
     }
 
     private fun guardar() {
@@ -58,16 +65,13 @@ class Inicio : AppCompatActivity() {
     private fun cambiarIdioma() {
         mediaPlayer.start()
         // Implementar lógica para cambiar el idioma
-        idiomaActual = if (idiomaActual == "es") {
-            // Si el idioma actual es español, cambiar a inglés
-            actualizarIdioma("en")
-            "en"
-        } else {
-            // Si el idioma actual no es español (puede ser inglés u otro), cambiar a español
-            actualizarIdioma("es")
-            "es"
+        if (idiomaActual != "es"){
+            idiomaActual = "es"
         }
-
+        else {
+            idiomaActual = "en"
+        }
+        LocaleHelper.setLocale(this,idiomaActual)
     }
     @SuppressLint("ResourceType")
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -98,15 +102,32 @@ class Inicio : AppCompatActivity() {
         val adapter = Agregar_producto(Lista_C)
         recyclerView.adapter = adapter
     }
-    private fun actualizarIdioma(idioma: String){
-        val recursos = resources
-        val displayMetrics = recursos.displayMetrics
-        val configuracion = resources.configuration
-        configuracion.setLocale(Locale(idioma))
-        recursos.updateConfiguration(configuracion, displayMetrics)
-        configuracion.locale = Locale(idioma)
-        resources.updateConfiguration(configuracion, displayMetrics)
+    object LocaleHelper{
+        fun setLocale(context: Context, language: String){
+            val newLocale = Locale(language)
+            Locale.setDefault(newLocale)
+            val resources = context.resources
+            val configuration = Configuration(resources.configuration)
+            configuration.setLocale(newLocale)
+            resources.updateConfiguration(configuration, resources.displayMetrics)
+            configuration.locale = newLocale
+            resources.updateConfiguration(configuration, resources.displayMetrics)
 
-        recreate()
+            /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
+                configuration.setLocale(newLocale)
+            }
+            else{
+                configuration.locale = newLocale
+            }
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                context.createConfigurationContext(configuration)
+            }
+            else {
+                resources.updateConfiguration(configuration, resources.displayMetrics)
+            }*/
+
+            (context as Activity).recreate()
+        }
     }
 }
---
+++ app/src/main/java/com/example/flores/compras/Lenguaje.kt
@@ -0,0 +1,6 @@
+package com.example.flores.compras
+
+import android.content.Context
+import android.content.res.Configuration
+import java.util.Locale
+import java.util.ResourceBundle
